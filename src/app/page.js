f6178951fc926390d9d25ec4c59a48f5c150434b
saadafahmed45 <hafijnahid45@gmail.com>
2023-07-27 07:01:35 +0600

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,12 @@ import Image from 'next/image'
 import Product from './products/page'
 import { Suspense } from 'react'
 import Loading from './loading'
+import Navbar from '@/components/Navbar'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen bg-purple-900 p-5 ">
-
+    <main className="h-screen overflow-auto bg-purple-900 p-5 ">
+      <Navbar />
 
       <Suspense fallback={<Loading />}>
         <Product />
